519041963d790ae45601bd1b81544af4bcc9ded5
RahulTC <rahul.adepu@techconsulting.tech>
2023-12-12 23:48:48 -0500

    completed assignment 21 part 2

---
+++ MVVMCSwiftUIAssign21/MVVMCSwiftUIAssign21/Coordinator/MainCoordinator.swift
@@ -0,0 +1,16 @@
+//
+//  MainCoordinator.swift
+//  MVVMCSwiftUIAssign21
+//
+//  Created by Rahul Adepu on 12/12/23.
+//
+
+import SwiftUI
+
+protocol Coordinator {
+    
+}
+
+class MainCoordinator {
+    
+}
