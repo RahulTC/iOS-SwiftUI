e2cce9fa1dec3ec5c55d5c8fd7b9d19be3ff3ad0
RahulTC <rahul.adepu@techconsulting.tech>
2023-12-13 08:41:05 -0500

    added FirbaseUIKit files again for some reason

---
+++ FirebaseUIKit/ProfileDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileDetailView.swift
+//  People
+//
+//  Created by Rahul Adepu on 12/11/23.
+//
+
+import SwiftUI
+
+struct ProfileDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileDetailView()
+}
